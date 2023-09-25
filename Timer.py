11924a7cd301510de4f3684ca5223fa4cf371d99
Kiavash <Kiavashtaherian@gmail.com>
2023-09-25 18:55:31 +0330

    base codes

--- Timer.py
+++ Timer.py
@@ -1,7 +1,5 @@
 import time
 from os import system, name, path
-from pydub import AudioSegment
-from pydub.playback import play
  
 while True:
 
@@ -11,22 +9,3 @@ while True:
       secends = int(input("Enter the Amount of secends: "))
       total_time = hours * 3600 + minutes * 60 + secends
       print("The Timer started...")
-      while total_time>=1:
-         print(total_time)
-         total_time -= 1
-         time.sleep(1)
-         if name == "nt":
-            system("cls")
-         else:
-            system("clear")
-      print("Your Time finished...")
-      song = AudioSegment.from_mp3(r"\\Timer\Time-Up.mp3")
-      print('playing sound using  pydub')
-      play(song)
-
-   elif "n" in choice.lower():
-      print('''Exiting...
-      bye!''')
-      break
-   else:
-      print("Invalid Input!")
