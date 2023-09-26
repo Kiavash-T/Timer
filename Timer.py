e53b81f6cdcd9966ac866abf4afafb3fb56de583
Kiavash <Kiavashtaherian@gmail.com>
2023-09-26 07:16:11 +0330

    clear system created

--- Timer.py
+++ Timer.py
@@ -9,3 +9,11 @@ while True:
       secends = int(input("Enter the Amount of secends: "))
       total_time = hours * 3600 + minutes * 60 + secends
       print("The Timer started...")
+      while total_time>=1:
+         print(total_time)
+         total_time -= 1
+         time.sleep(1)
+         if name == "nt":
+            system("cls")
+         else:
+            system("clear")
